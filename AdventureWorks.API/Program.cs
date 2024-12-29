f69e080f632bac37caba348b6abc849d4913b72c
Edwin Benalcazar <softwareywebsoluciones@gmail.com>
2024-12-29 00:58:24 -0500

    feat: setup DB using Scaffolding  ef core and add connection as DI

--- AdventureWorks.API/Program.cs
+++ AdventureWorks.API/Program.cs
@@ -1,3 +1,5 @@
+using AdventureWorks.Infrastructure;
+
 WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +8,8 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddInfrastructure(builder.Configuration);
 
 WebApplication app = builder.Build();
 
---
+++ AdventureWorks.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,22 @@
+﻿using AdventureWorks.Infrastructure.Persistence.DbContexts;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace AdventureWorks.Infrastructure;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddInfrastructure(
+        this IServiceCollection services,
+        IConfiguration configuration)
+    {
+        string connectionString = configuration.GetConnectionString("Database") ??
+                                  throw new ArgumentNullException(nameof(configuration));
+
+        services.AddDbContext<AdventureWorks2022Context>(options =>
+            options.UseSqlServer(connectionString));
+
+        return services;
+    }
+}
