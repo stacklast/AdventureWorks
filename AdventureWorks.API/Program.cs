2a038a4e8aeced8b53665f152660e0e48265f53a
Edwin Benalcazar <softwareywebsoluciones@gmail.com>
2024-12-30 22:50:21 -0500

    feat: Enable SwaggerUI for Production.

--- AdventureWorks.API/Program.cs
+++ AdventureWorks.API/Program.cs
@@ -17,12 +17,11 @@ builder.Services.AddInfrastructure(builder.Configuration);
 WebApplication app = builder.Build();
 
 app.UseSwagger();
+app.UseSwaggerUI();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwaggerUI();
-
     app.ApplyMigrations();
 }
 
